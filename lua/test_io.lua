39b18c16fc0743d4a795a0345be1bce83bfbd9eb
kcruci <yjluan@gmail.com>
2016-06-02 16:00:06 +0800

    lua

--- lua/test_io.lua
+++ lua/test_io.lua
@@ -1,7 +1,30 @@
 filename = "test_io.data"
 
-io.input("test_io.data")
 
+print("\n#### buffer read open read all ####")
+f = io.open(filenam)
+BUFFSIZE =5
+local cc, lc, wc = 0, 0, 0  --分别计数字符、行和单词
+while true do
+    local lines,rest = f:read(BUFFSIZE,"*line")
+    if not lines then
+        break
+    end
+
+    if rest then
+        lines = lines .. rest .. "\n"
+    end
+end
+--[[
+
+print("\n#### ####")
+local str = "\n\n\ndddddf 123 fff f\nd"
+print(str, #str)
+print("\n#### ####")
+--]]
+
+print("\n#### ####")
+io.input("test_io.data")
 for count = 1,math.huge do
     local line = io.read("*line")
     if line == "" then
@@ -15,6 +38,7 @@ end
     下面的代码读取注释中的数字
 --]]
 -- seek the beginner
+print("\n#### read number ####")
 io.input("test_io.data")
 while true do
     local n1,n2,n3 = io.read("*number","*number","*number")
@@ -24,12 +48,22 @@ while true do
     print(math.max(n1,n2,n3))
 end
 
---io.input("test_io.data")
-print("#### read all ####")
+io.input("test_io.data")
+
+print("\n#### read all ####")
 local all= io.read("*all")
 io.write(all)
 
+print("\n#### function ####")
 foo = function (x) return 2*x end
 print(foo(22))
 
 --for name in pairs(_G) do print(_G) end
+
+print("\n#### file open read all ####")
+local f = io.open(filename, "r")
+t = f:read("*all")
+print(t)
+
+
+
